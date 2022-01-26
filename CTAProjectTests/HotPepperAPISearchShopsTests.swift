6b73fddbb32fb35d730823acf046543a2ce119a0
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-01-27 00:13:18 +0900

    Update HotPepperAPISearchShopsTests

--- CTAProjectTests/HotPepperAPISearchShopsTests.swift
+++ CTAProjectTests/HotPepperAPISearchShopsTests.swift
@@ -11,46 +11,130 @@ import XCTest
 @testable import RxSwift
 
 class HotPepperAPISearchShopsTests: XCTestCase {
+    static let keyword = "焼き肉"
     let disposeBag = DisposeBag()
-    let targetType = HotPepperAPI.Request.SearchShops(keyword: "焼き肉")
+    let targetType = HotPepperAPI.Request.SearchShops(keyword: keyword)
     
-    func testSuccess() {
-        let stubbingProvider = MoyaProvider<MultiTarget>(stubClosure: MoyaProvider.immediatelyStub)
-        
-        XCTAssertEqual(targetType.queryParameters["name_any"], "焼き肉")
-        
-        APIClient.shared
-            .send(provider: stubbingProvider, targetType).subscribe(onNext: { result in
-                switch(result) {
-                case .success(let response):
-                    print(self.targetType.path)
-                    XCTAssertEqual(response.results.shop[0].name, "もつ鍋 焼き肉 岩見 西新店")
-                default:
-                    XCTFail("failed: \(result)")
-                }
-            }).disposed(by: disposeBag)
-    }
+    typealias Response = HotPepperAPI.Response
+    typealias SearchShops = HotPepperAPI.SearchShopsModel
+    typealias ErrorModel = HotPepperAPI.ErrorModel
     
-    func testAPIError() {
-        let customEndpointClosure = { (target: MultiTarget) -> Endpoint in
-            return Endpoint(url: URL(target: target).absoluteString,
-                            sampleResponseClosure: { .networkResponse(3000, HotPepperAPI.ErrorModel.exampleJSON.data(using: .utf8)! )},
-                            method: target.method,
-                            task: target.task,
-                            httpHeaderFields: target.headers)
+    func test_APIResponseSuccess() {
+        let endpointClosure = { (target: MultiTarget) -> Endpoint in
+            return Self.generateEndpointClosure(
+                target: target,
+                sampleResponse: .response(
+                    Self.httpURLResponse(target: target, statusCode: 3000),
+                    SearchShops.exampleJSON.data(using: .utf8)!
+                )
+            )
         }
-        let stubbingProvider = MoyaProvider<MultiTarget>(endpointClosure: customEndpointClosure, stubClosure: MoyaProvider.immediatelyStub)
+        let stubbingProvider = MoyaProvider<MultiTarget>(endpointClosure: endpointClosure, stubClosure: MoyaProvider.immediatelyStub)
+        XCTAssertEqual(targetType.queryParameters["name_any"], Self.keyword)
         
         APIClient.shared
-            .send(provider: stubbingProvider, targetType).subscribe(onNext: { result in
-                switch(result) {
-                case .error(let error):
-                    XCTAssertEqual(error.results.error[0].code, 3000)
-                    XCTAssertEqual(error.results.error[0].message, "少なくとも１つの条件を入れてください。")
-                default:
-                    XCTFail("failed: \(result)")
+            .send(provider: stubbingProvider, targetType).subscribe(
+                onSuccess: { result in
+                    switch result {
+                    case .success(let response):
+                        XCTAssertEqual(response.results.shop[0].name, "もつ鍋 焼き肉 岩見 西新店")
+                    default:
+                        XCTFail("failed: \(result)")
+                    }
                 }
-            }).disposed(by: disposeBag)
+            )
+            .disposed(by: disposeBag)
     }
     
+    func test_APIResponseError() {
+        let endpointClosure = { (target: MultiTarget) -> Endpoint in
+            return Self.generateEndpointClosure(
+                target: target,
+                sampleResponse: .response(
+                    Self.httpURLResponse(target: target, statusCode: 3000),
+                    ErrorModel.exampleJSON.data(using: .utf8)!
+                )
+            )
+        }
+        let stubbingProvider = MoyaProvider<MultiTarget>(endpointClosure: endpointClosure, stubClosure: MoyaProvider.immediatelyStub)
+        
+        APIClient.shared
+            .send(provider: stubbingProvider, targetType).subscribe(
+                onSuccess: { result in
+                    switch result {
+                    case .apiError(let response):
+                        XCTAssertEqual(response.results.error[0].code, 3000)
+                        XCTAssertEqual(response.results.error[0].message, "少なくとも１つの条件を入れてください。")
+                    default:
+                        XCTFail()
+                    }
+                },
+                onFailure: { error in
+                    XCTFail()
+                }).disposed(by: disposeBag)
+    }
+    
+    func test_APIResponseMoyaError() {
+        let endpointClosure = { (target: MultiTarget) -> Endpoint in
+            return Self.generateEndpointClosure(
+                target: target,
+                sampleResponse: .networkError(NSError(domain: "networkError", code: 300, userInfo: nil)))
+        }
+        let stubbingProvider = MoyaProvider<MultiTarget>(endpointClosure: endpointClosure, stubClosure: MoyaProvider.immediatelyStub)
+        
+        APIClient.shared
+            .send(provider: stubbingProvider, targetType).subscribe(
+                onSuccess: { result in
+                    XCTFail()
+                },
+                onFailure: { error in
+                    XCTAssertNoThrow(error as! MoyaError)
+                }).disposed(by: disposeBag)
+    }
+    
+    func test_APIResponseUnexpectedError() {
+        let endpointClosure = { (target: MultiTarget) -> Endpoint in
+            return Self.generateEndpointClosure(
+                target: target,
+                sampleResponse: .response(
+                    Self.httpURLResponse(target: target, statusCode: 3000),
+                    "data".data(using: .utf8)!
+                )
+            )
+        }
+        
+        let stubbingProvider = MoyaProvider<MultiTarget>(endpointClosure: endpointClosure, stubClosure: MoyaProvider.immediatelyStub)
+        
+        APIClient.shared
+            .send(provider: stubbingProvider, targetType).subscribe(
+                onSuccess: { result in
+                    XCTFail()
+                },
+                onFailure: { error in
+                    XCTAssertNoThrow(error as! UnexpectedError)
+                }).disposed(by: disposeBag)
+    }
+}
+
+extension HotPepperAPISearchShopsTests {
+    static func generateEndpointClosure(target: MultiTarget, sampleResponse: EndpointSampleResponse) -> Endpoint {
+        return Endpoint(
+            url: URL(target: target).absoluteString,
+            sampleResponseClosure: {
+                sampleResponse
+            },
+            method: target.method,
+            task: target.task,
+            httpHeaderFields: target.headers
+        )
+    }
+    
+    static func httpURLResponse(target: MultiTarget, statusCode: Int) -> HTTPURLResponse {
+        return HTTPURLResponse(
+            url: URL(target: target),
+            statusCode: 3000,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+    }
 }
