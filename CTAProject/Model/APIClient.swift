f3d0803876db6a91d84ef93b1c197777f266005e
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-01-27 00:12:00 +0900

    Update APIClient

--- CTAProject/Model/APIClient.swift
+++ CTAProject/Model/APIClient.swift
@@ -9,46 +9,41 @@ import Foundation
 import Moya
 import RxSwift
 
-
+/// @mockable
 protocol APIClientProtocol {
-    func send<Request: BaseTargetType>(provider: MoyaProvider<MultiTarget>, _ request: Request) -> Observable<APIResult<Request.Response, Request.ErrorResponse>>
+    func send<Request: BaseTargetType>(provider: MoyaProvider<MultiTarget>, _ request: Request) -> Single<APIResult<Request.Response, Request.ErrorResponse>>
 }
 
 final class APIClient: APIClientProtocol {
     static let shared = APIClient()
     private let provider = MoyaProvider<MultiTarget>()
-    private let decoder: JSONDecoder = {
-    let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        return decoder
-    }()
     
-    func send<Request: BaseTargetType>(provider: MoyaProvider<MultiTarget> = MoyaProvider<MultiTarget>(),_ request: Request) -> Observable<APIResult<Request.Response, Request.ErrorResponse>> {
-        Observable.create { [self] observer in
-            return provider.rx.request(MultiTarget(request))
-                .filterSuccessfulStatusCodes()
-                .map(Request.Response.self, using: self.decoder)
-                .subscribe { event in
-                    switch event {
-                    case .success(let response):
-                        observer.onNext(.success(response))
-                    case .failure(let error):
-                        if let moyaError = error as? MoyaError {
-                            switch moyaError {
-                            case .statusCode(let response):
-                                do {
-                                    let errorResponse = try self.decoder.decode(Request.ErrorResponse.self, from: response.data)
-                                    observer.onNext(.error(errorResponse))
-                                } catch {
-                                    observer.onError(error)
-                                }
-                            default:
-                                observer.onError(error)
-                            }
-                        }
+    func send<Request: BaseTargetType>(provider: MoyaProvider<MultiTarget> = MoyaProvider<MultiTarget>(),_ request: Request) -> Single<APIResult<Request.Response, Request.ErrorResponse>> {
+        return provider.rx.request(MultiTarget(request))
+            .flatMap { (result) -> Single<APIResult<Request.Response, Request.ErrorResponse>> in
+                guard (200...299).contains(result.statusCode) else {
+                    do {
+                        let apiError = try result.map(Request.ErrorResponse.self)
+                        return .just(.apiError(apiError))
+                    } catch {
+                        return .error(UnexpectedError())
                     }
                 }
-        }
+                do {
+                    let response = try result.map(Request.Response.self, using: APIClient.decoder)
+                    return .just(.success(response))
+                } catch {
+                    return .error(UnexpectedError())
+                }
+            }
     }
 }
 
+
+extension APIClient {
+    static var decoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
+}
