00a9daede58118fa02587bc429ed9f397a8d5e03
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-01-31 23:13:20 +0900

    Update SearchShopsAlertModal

--- CTAProject/UIComponent/Components/SearchShopsAlertModal.swift
+++ CTAProject/UIComponent/Components/SearchShopsAlertModal.swift
@@ -13,17 +13,20 @@ final class SearchShopsAlertModal: UIView {
     
     private let containerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .white
+        view.backgroundColor = Asset.baseBackgroundColor.color
         return view
     }()
     
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = L10n.searchShopsAlertModalLabelText
+        label.textAlignment = .center
         label.textColor = .black
         label.font = UIFont.systemFont(ofSize: Const.TitleLabel.fontSize)
         label.numberOfLines = Const.TitleLabel.numberOfLines
-        return label
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+        return  label
     }()
     
     private let closeButton: UIButton = {
@@ -59,20 +62,22 @@ final class SearchShopsAlertModal: UIView {
 
 //MARK: Constants
 extension SearchShopsAlertModal {
-    struct Const {
-        struct ContainerView {
+    enum Const {
+        enum ContainerView {
             static let marginLeft = 20
             static let marginRight = -20
             static let heightRatio = 3.5
         }
         
-        struct TitleLabel {
+        enum TitleLabel {
             static let fontSize: CGFloat = 20
             static let marginTop = 48
-            static let numberOfLines = 0
+            static let marginRight = -8
+            static let marginLeft = 8
+            static let numberOfLines = 1
         }
         
-        struct CloseButton {
+        enum CloseButton {
             static let fontSize: CGFloat = 16
             static let marginBottom = -30
             static let size = CGSize(width: 180, height: 48)
@@ -84,7 +89,7 @@ extension SearchShopsAlertModal {
 //MARK: UI Configuration
 extension SearchShopsAlertModal {
     private func setupView() {
-        backgroundColor = .searchShopsAlertModalBackground
+        backgroundColor = Asset.modalOverlay.color
         addSubview(containerView)
         containerView.addSubview(titleLabel)
         containerView.addSubview(closeButton)
@@ -109,9 +114,13 @@ extension SearchShopsAlertModal {
         titleLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(containerView).offset(Const.TitleLabel.marginTop)
+            make.left.equalTo(containerView.snp.left).offset(Const.TitleLabel.marginLeft)
+            make.right.equalTo(containerView.snp.right).offset(Const.TitleLabel.marginRight)
+            make.width.equalTo(containerView.snp.width)
         }
         closeButton.snp.makeConstraints { make in
             make.size.equalTo(Const.CloseButton.size)
+            make.top.equalTo(titleLabel.snp.bottom).offset(16)
             make.bottom.equalTo(containerView).offset(Const.CloseButton.marginBottom)
             make.centerX.equalToSuperview()
         }
