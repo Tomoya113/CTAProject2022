11da65119a1a64aa5a6b7ea11f25fcb537e85ca9
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-02-18 00:54:02 +0900

    Update SearchShopsViewController

--- CTAProject/UIComponent/ViewController/SearchShopsViewController.swift
+++ CTAProject/UIComponent/ViewController/SearchShopsViewController.swift
@@ -5,14 +5,29 @@
 //  Created by Tomoya Tanaka on 2022/01/12.
 //
 
-import UIKit
+import Moya
+import PKHUD
+import RxCocoa
+import RxSwift
 import SnapKit
+import UIKit
 
 final class SearchShopsViewController: UIViewController {
-    
+
+    private let disposeBag = DisposeBag()
+
+    private let viewModel: SearchShopsViewModel = {
+        let provider = MoyaProvider<MultiTarget>()
+        let viewModel = SearchShopsViewModel(
+            model: SearchShopsModel(provider: provider)
+        )
+        return viewModel
+    }()
+
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.backgroundColor = .systemGray6
+        tableView.rowHeight = Const.TableViewCell.height
         tableView.register(SearchShopsTableViewCell.self, forCellReuseIdentifier: SearchShopsTableViewCell.reuseIdentifier)
         return tableView
     }()
@@ -22,19 +37,76 @@ final class SearchShopsViewController: UIViewController {
         searchBar.placeholder = L10n.searchShopsSearchBarPlaceholder
         return searchBar
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        setDelegates()
-    }
 
-    //NOTE: navigationBarを参照する必要があるので、viewDidAppearでAutoLayoutの設定を呼んでいます
+        searchBar.rx.searchButtonClicked
+            .subscribe(onNext: { [searchBar] _ in
+                searchBar.resignFirstResponder()
+                searchBar.endEditing(true)
+            })
+            .disposed(by: disposeBag)
+
+        let searchBarText = searchBar.rx.textDidEndEditing
+            .withLatestFrom(searchBar.rx.text.orEmpty.asObservable())
+            .distinctUntilChanged()
+            .filter { !$0.isEmpty }
+            .asDriver(onErrorJustReturn: "")
+
+        searchBarText
+            .drive(onNext: { [searchBar] _ in
+                searchBar.text = ""
+            })
+            .disposed(by: disposeBag)
+
+        // NOTE: 普通に文字が流れてきた時
+        searchBarText
+            .filter { $0.count < 50 }
+            .drive(onNext: { [weak self] text in
+                guard let self = self else { return }
+                self.viewModel.inputs.keyword.onNext(text)
+            })
+            .disposed(by: disposeBag)
+
+        // NOTE: 50文字以上
+        searchBarText
+            .filter { $0.count > 50 }
+            .drive(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                self.view.addSubview(SearchShopsAlertModal())
+            })
+            .disposed(by: disposeBag)
+
+        viewModel.outputs.shops
+            .asObservable()
+            .bind(to: tableView.rx.items(
+                cellIdentifier: SearchShopsTableViewCell.reuseIdentifier,
+                cellType: SearchShopsTableViewCell.self)
+            ) { index, shop, cell in
+                cell.configureCell(
+                    shopName: shop.name,
+                    locationName: shop.stationName,
+                    price: shop.budget.name,
+                    shopImageURL: shop.logoImage
+                )
+            }
+            .disposed(by: disposeBag)
+
+        viewModel.outputs.loading
+            .drive(onNext: { result in
+                result ? HUD.show(.progress) : HUD.hide()
+            })
+            .disposed(by: disposeBag)
+
+    }
+    // NOTE: navigationBarを参照する必要があるので、viewDidAppearでAutoLayoutの設定を呼んでいます
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         makeConstraints()
     }
-    
+
 }
 
 //MARK: Constants
@@ -44,49 +116,14 @@ extension SearchShopsViewController {
             static let height = 64
             static let maxWordCount = 50
         }
-        
+
         enum TableViewCell {
             static let height: CGFloat = 192
         }
     }
 }
 
-//MARK: TableView Configuration
-extension SearchShopsViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: SearchShopsTableViewCell.reuseIdentifier, for: indexPath) as! SearchShopsTableViewCell
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return Const.TableViewCell.height
-    }
-}
-
-//MARK: SearchBar Configuration
-extension SearchShopsViewController: UISearchBarDelegate {
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
-        guard let searchBarText = searchBar.text else {
-            return
-        }
-        
-        guard searchBarText.count <= Const.SearchBar.maxWordCount else {
-            view.addSubview(SearchShopsAlertModal())
-            searchBar.text = ""
-            return
-        }
-        
-        //TODO: 店の情報を取得する
-    }
-    
-}
-
-//MARK: UI Configuration
+// MARK: UI Configuration
 extension SearchShopsViewController {
     private func setupView() {
         view.backgroundColor = .white
@@ -94,15 +131,10 @@ extension SearchShopsViewController {
         view.addSubview(searchBar)
         navigationItem.title = L10n.searchShopsNavigationBarTitle
     }
-    
-    private func setDelegates() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        searchBar.delegate = self
-    }
+
 }
 
-//MARK: AutoLayout Configuration
+// MARK: AutoLayout Configuration
 extension SearchShopsViewController {
     private func makeConstraints() {
         searchBar.snp.makeConstraints { make in
@@ -118,6 +150,8 @@ extension SearchShopsViewController {
             make.width.equalTo(view)
             make.top.equalTo(searchBar.snp.bottom)
             make.bottom.equalTo(view.snp.bottom)
+            make.leading.equalTo(view.snp.leading)
+            make.trailing.equalTo(view.snp.trailing)
         }
     }
 }
