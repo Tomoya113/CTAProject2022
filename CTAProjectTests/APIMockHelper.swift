3454280902d3715de3053c24a920d0399fab6830
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-02-18 00:54:51 +0900

    Add APIMockHelper

---
+++ CTAProjectTests/APIMockHelper.swift
@@ -0,0 +1,32 @@
+//
+//  APIMockHelper.swift
+//  CTAProjectTests
+//
+//  Created by Tomoya Tanaka on 2022/02/09.
+//
+
+import Foundation
+@testable import Moya
+
+enum APIMockHelper {
+    static func generateEndpointClosure(target: MultiTarget, sampleResponse: EndpointSampleResponse) -> Endpoint {
+        return Endpoint(
+            url: URL(target: target).absoluteString,
+            sampleResponseClosure: {
+                sampleResponse
+            },
+            method: target.method,
+            task: target.task,
+            httpHeaderFields: target.headers
+        )
+    }
+
+    static func httpURLResponse(target: MultiTarget, statusCode: Int) -> HTTPURLResponse {
+        return HTTPURLResponse(
+            url: URL(target: target),
+            statusCode: statusCode,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+    }
+}
