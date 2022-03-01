50a4d7f9dcce4ea76b7707a3b1dd5dca237743db
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-03-01 21:37:13 +0900

    Update SearchShopsModelTests

--- CTAProjectTests/SearchShopsModelTests.swift
+++ CTAProjectTests/SearchShopsModelTests.swift
@@ -8,31 +8,44 @@
 @testable import CTAProject
 @testable import Moya
 @testable import RxSwift
+@testable import RxTest
 import XCTest
 
 class SearchShopsModelTests: XCTestCase {
     typealias SearchShops = HotPepperAPI.SearchShopsModel
-    let disposeBag = DisposeBag()
 
     // mockoloで生成されたAPIClientの型がAnyになるので、一旦mockoloを使わずにmockを作っています。
     func test_fetchShops() throws {
-        let endpointClosure = { (target: MultiTarget) -> Endpoint in
-            return APIMockHelper.generateEndpointClosure(
-                target: target,
-                sampleResponse: .response(
-                    APIMockHelper.httpURLResponse(target: target, statusCode: 200),
-                    SearchShops.exampleJSON.data(using: .utf8)!
+        let dependency = Dependency()
+        let fetchShops = WatchStream(dependency.testTarget.fetchShops(keyword: "dummy").asObservable())
+        dependency.testScheduler.start()
+        // NOTE: WatchStreamのvalueを使うとcompletedが返ってきてしまうのでeventsをmapしています
+        let fetchShopsResult = fetchShops.observer.events.map { $0.value.element }
+        XCTAssertEqual(fetchShopsResult.first??.first?.name, "もつ鍋 焼き肉 岩見 西新店")
+    }
+}
+
+extension SearchShopsModelTests {
+    struct Dependency {
+        let testScheduler: TestScheduler
+        let testTarget: SearchShopsModelType
+        let stubbingProvider: MoyaProvider<MultiTarget>
+
+        init() {
+            testScheduler = TestScheduler(initialClock: 0)
+            let endpointClosure = { (target: MultiTarget) -> Endpoint in
+                return APIMockHelper.generateEndpointClosure(
+                    target: target,
+                    sampleResponse: .response(
+                        APIMockHelper.httpURLResponse(target: target, statusCode: 200),
+                        SearchShops.exampleJSON.data(using: .utf8)!
+                    )
                 )
-            )
+            }
+            stubbingProvider = MoyaProvider<MultiTarget>(endpointClosure: endpointClosure, stubClosure: MoyaProvider.immediatelyStub)
+
+            let searchShopsRepository = SearchShopsRepository(provider: stubbingProvider)
+            testTarget = SearchShopsModel(searchShopsRepository: searchShopsRepository)
         }
-        let stubbingProvider = MoyaProvider<MultiTarget>(endpointClosure: endpointClosure, stubClosure: MoyaProvider.immediatelyStub)
-
-        let model = SearchShopsModel(provider: stubbingProvider)
-
-        model.fetchShops(keyword: "dummy")
-            .subscribe(onNext: { shops in
-                XCTAssertEqual(shops[0].name, "もつ鍋 焼き肉 岩見 西新店")
-            })
-            .disposed(by: disposeBag)
     }
 }
