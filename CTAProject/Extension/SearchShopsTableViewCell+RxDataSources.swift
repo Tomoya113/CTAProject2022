b511c568cc9816e7ef8147b9133cb539e145569a
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-03-08 21:21:47 +0900

    Add SearchShopsTableViewCell+RxDataSources

---
+++ CTAProject/Extension/SearchShopsTableViewCell+RxDataSources.swift
@@ -0,0 +1,34 @@
+//
+//  SearchShopsTableViewCell.swift
+//  CTAProject
+//
+//  Created by Tomoya Tanaka on 2022/03/08.
+//
+
+import RxDataSources
+
+typealias SearchShopsTableViewSection = SearchShopsTableViewCell.SearchShopsTableViewSection
+typealias SearchShopsTableViewCellData = SearchShopsTableViewCell.SearchShopsTableViewCellData
+
+extension SearchShopsTableViewCell {
+    struct SearchShopsTableViewCellData {
+        let shopName: String
+        let locationName: String
+        let price: String
+        let shopImageURL: String
+        let favorited: Bool
+    }
+
+    struct SearchShopsTableViewSection {
+        var items: [SearchShopsTableViewCellData]
+    }
+
+}
+
+extension SearchShopsTableViewSection: SectionModelType {
+    typealias Item = SearchShopsTableViewCellData
+    init (original: Self, items: [Item]) {
+        self = original
+        self.items = items
+    }
+}
