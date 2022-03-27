2b29beb34c199620523c441f68c89975f02d8259
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-03-27 18:38:12 +0900

    Update SearchShopsViewModel

--- CTAProject/UILogic/SearchShops/SearchShopsViewModel.swift
+++ CTAProject/UILogic/SearchShops/SearchShopsViewModel.swift
@@ -13,13 +13,15 @@ import RxSwift
 // NOTE: 外部から受ける命令
 protocol SearchShopsViewModelInputs {
     var searchWord: AnyObserver<String> { get }
+    var tapFavoriteButton: AnyObserver<IndexPath> { get }
 }
 
 // NOTE: 外部に渡す値
 protocol SearchShopsViewModelOutputs: SearchShops {
-    var shops: Driver<Shops> { get }
+    var shops: Driver<[SearchShopsTableViewCellData]> { get }
     var loading: Driver<Bool> { get }
     var hasSearchWordCountExceededError: Driver<Bool> { get }
+    var didPressFavoriteButton: Driver<IndexPath> { get }
 }
 
 protocol SearchShopsViewModelType {
@@ -30,26 +32,40 @@ protocol SearchShopsViewModelType {
 final class SearchShopsViewModel: SearchShopsViewModelInputs, SearchShopsViewModelOutputs {
 
     var searchWord: AnyObserver<String>
-    var shops: Driver<Shops>
+    var tapFavoriteButton: AnyObserver<IndexPath>
+    var shops: Driver<[SearchShopsTableViewCellData]>
     var loading: Driver<Bool>
     var hasSearchWordCountExceededError: Driver<Bool>
+    var didPressFavoriteButton: Driver<IndexPath>
 
     private var disposeBag = DisposeBag()
 
     init(model: SearchShopsModelType) {
         let _searchWord = PublishRelay<String>()
-        let _shops = BehaviorRelay<Shops>(value: [])
+        let _tapFavoriteButton = PublishRelay<IndexPath>()
+
+        let _shops = BehaviorRelay<[SearchShopsTableViewCellData]>(value: [])
         let _loading = PublishRelay<Bool>()
         let _hasSearchWordCountExceededError = PublishRelay<Bool>()
+        let _didPressFavoriteButton = PublishRelay<IndexPath>()
 
+        // MARK: Input
         searchWord = AnyObserver<String> { event in
             guard let keyword = event.element else { return }
             _searchWord.accept(keyword)
         }
+        tapFavoriteButton = AnyObserver<IndexPath> { event in
+            guard let tapFavoriteButton = event.element else { return }
+            _tapFavoriteButton.accept(tapFavoriteButton)
+        }
+
+        // MARK: Output
         shops = _shops.asDriver()
         loading = _loading.asDriver(onErrorJustReturn: false)
         hasSearchWordCountExceededError = _hasSearchWordCountExceededError.asDriver(onErrorDriveWith: .empty())
+        didPressFavoriteButton = _didPressFavoriteButton.asDriver(onErrorDriveWith: .empty())
 
+        // MARK: SearchQuery
         let searchWordValidationResult = _searchWord.flatMap { searchWord -> Observable<SearchWordValidator.ValidationResult> in
             return .just(SearchWordValidator.validate(searchWord))
         }
@@ -62,7 +78,7 @@ final class SearchShopsViewModel: SearchShopsViewModelInputs, SearchShopsViewMod
 
         let searchResult = searchWordValidationResult
             .filterValid()
-            .flatMap { searchWord -> Single<Shops> in
+            .flatMap { searchWord -> Single<[SearchShopsTableViewCellData]> in
                 _loading.accept(true)
                 return model.fetchShops(keyword: searchWord)
             }
@@ -75,6 +91,40 @@ final class SearchShopsViewModel: SearchShopsViewModelInputs, SearchShopsViewMod
 
         searchResult
             .bind(to: _shops)
+            .disposed(by: disposeBag)
+
+        // MARK: OnSelectedCell
+        let selectedCell = _tapFavoriteButton.flatMap { indexPath -> Observable<(SearchShopsTableViewCellData, IndexPath)> in
+            let shop = _shops.value[indexPath.row]
+            return Observable.combineLatest(Observable.just(shop), Observable.just(indexPath))
+        }
+        .share()
+
+        selectedCell
+            .filter { (shop, _) in !shop.favorited }
+            .flatMap { (shop, indexPath) -> Single<IndexPath> in
+                return model.addFavoriteShop(FavoriteShop(shop))
+                    .map { _ in return indexPath }
+            }
+            .bind(to: _didPressFavoriteButton)
+            .disposed(by: disposeBag)
+
+        selectedCell
+            .filter { (shop, _) in shop.favorited }
+            .flatMap { (shop, indexPath) -> Single<IndexPath> in
+                return model.removeFavoriteShop(shop.id)
+                    .map { _ in return indexPath }
+            }
+            .bind(to: _didPressFavoriteButton)
+            .disposed(by: disposeBag)
+
+        _didPressFavoriteButton
+            .map { indexPath in
+                var shops = _shops.value
+                shops[indexPath.row].favorited = !shops[indexPath.row].favorited
+                _shops.accept(shops)
+            }
+            .subscribe()
             .disposed(by: disposeBag)
 
     }
